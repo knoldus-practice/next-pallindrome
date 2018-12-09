ad1be1595d7f5f2ee620c712cc0fd40df7c95b58
saswata-dutta <saswat.dutta@gmail.com>
2018-12-09 22:23:04 +0530

    impl the nextPallindrome function

--- src/main/scala/example/Main.scala
+++ src/main/scala/example/Main.scala
@@ -1,11 +1,23 @@
 package example
 
+import java.math.BigInteger
+
 object Main {
   def main(args: Array[String]): Unit = {
 
   }
 
-  def nextPallindrome(number: String): String = {
-    ???
+  def nextPallindrome(num: String): String = {
+    val len = num.length
+    val left = num.substring(0, len / 2)
+    val middle = num.substring(len / 2, len - len / 2)
+    val right = num.substring(len - len / 2)
+
+    if (right.compareTo(left.reverse) < 0) {
+      left + middle + left.reverse
+    } else {
+      val next = new BigInteger(left + middle).add(BigInteger.ONE).toString
+      next.substring(0, left.length + middle.length) + next.reverse.substring(middle.length)
+    }
   }
 }
